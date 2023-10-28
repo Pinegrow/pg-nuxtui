ec11f2ddfe83073cb8a3487f1962c2e013649956
Akayy <techakayy@gmail.com>
2023-10-28 22:39:05 +1100

    Using simple img tag for rating stars

--- components/StarsRate.vue
+++ components/StarsRate.vue
@@ -14,8 +14,8 @@
 </script>
 <template>
   <div class="aspect-auto relative">
-    <NuxtImg src="/stars.webp" class="absolute inset-0" aria-hidden="true" />
-    <NuxtImg
+    <img src="/stars.webp" class="absolute inset-0" aria-hidden="true" />
+    <img
       src="/stars-filled.webp"
       class="absolute inset-0"
       aria-hidden="true"
