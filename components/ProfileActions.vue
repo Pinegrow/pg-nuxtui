72689f98e91cf7426badf49c4b0ac428ee371557
Akayy <techakayy@gmail.com>
2023-10-29 22:43:41 +1100

    Minor edit

--- components/ProfileActions.vue
+++ components/ProfileActions.vue
@@ -1,19 +1,5 @@
 <script setup lang="ts">
-  // const props = defineProps({
-  // 	name: {
-  // 		type: String,
-  // 		default: 'ProfileActions',
-  // 	},
-  // })
-
-  // const emit = defineEmits(['addToCart'])
-
-  // // Use the addToCart function like this:
-  // // Eg-1: addToCart('pizza', 10)
-  // // Eg-2: (event) => addToCart('pizza', 10, event)
-  // const addToCart = (item, quantity /*, event*/) => {
-  // 	emit('addToCart', { item, quantity })
-  // }
+  // ...
 </script>
 <template>
   <div class="flex flex-nowrap items-center justify-center">
