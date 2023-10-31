b2a669d1c10a00512f15fd6ac5a7dfe3da806bc9
Akayy <techakayy@gmail.com>
2023-10-31 21:04:34 +1100

    Centered

--- pages/index.vue
+++ pages/index.vue
@@ -35,9 +35,21 @@
   const bgStyles = heroImageOptimized.bgStyles
 </script>
 <template>
-  <main>
-    <span>A Fresh Start!</span>
-    <DarkModeSwitch />
-  </main>
+  <div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <DarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
+        >A Fresh Start!</span
+      >
+    </div>
+  </div>
 </template>
 <style scoped></style>
