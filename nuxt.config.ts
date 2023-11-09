bf755d0e3d4ae93150e6d96a9049e840efd2f0ce
Akayy <techakayy@gmail.com>
2023-11-10 09:12:47 +1100

    Updated unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -265,16 +265,6 @@ export default defineNuxtConfig({
   //       prefix: 'i-', // default prefix, do not change
   //     }),
   //   ],
-  //   content: {
-  //     pipeline: {
-  //       include: [
-  //         /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/, // the default
-  //         './*.{js,cjs,ts,cts,mts}', // site.ts
-  //         '*/**/*.{js,cjs,ts,cts,mts}', // nuxt directories
-  //         '*-layer/**/*.{js,cjs,ts,cts,mts}', // nuxt layers
-  //       ],
-  //     },
-  //   },
   // },
 
   pinegrow: {
