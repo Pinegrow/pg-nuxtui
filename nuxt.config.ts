6f63bb30291366cee95e6241393f35ff64bddaab
Akayy <techakayy@gmail.com>
2023-11-09 17:21:52 +1100

    Added my-nuxtui-layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,16 @@ const {
 } = site
 
 export default defineNuxtConfig({
+  extends: [
+    './my-nuxtui-layer', // NavBar and Footer components
+  ],
+  components: [
+    {
+      path: '~/components/OgImage',
+      island: true,
+    },
+    '~/components',
+  ],
   // ssr: false,
   devtools: { enabled: false }, // Disable when using Vue devtools
 
