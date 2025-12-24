82c48cdb3a907524ea12fe9a6dde5bda0eeceb8d
Akayy <techakayy@gmail.com>
2025-12-24 23:53:12 +1100

    Updated image url

--- server/api/store/local-data/data.ts
+++ server/api/store/local-data/data.ts
@@ -252,7 +252,7 @@ export default [
     description:
       "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
     category: 'women',
-    image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+    image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2t.png',
     rating: {
       rate: 9.8,
       count: 679,
